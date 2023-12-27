2df1aada80acc8936869332947a897aee111ea34
Adam <olmpy@hotmail.com>
2023-12-26 18:08:53 -0700

    edit PhraseOMatic.java

--- PhraseOMatic.java
+++ PhraseOMatic.java
@@ -5,11 +5,22 @@
 	About: An app that makes 3 lists of of words and then randomly picks up one word from each of the three lists, then print out the results to the console. 
 */
 public class PhraseOMatic {
-	Public static void main(String []args) {
-		String[] wordListOne = {"Fido", "Zues", "Jesus"};
-		String[] wordListTwo = {"Saski", "Naruto", "Gara"};
+	public static void main(String []args) {
+		String[] wordListOne = {"Artemis", "Zeus", "Jesus"};
+		String[] wordListTwo = {"Sasuki", "Naruto", "Gara"};
 		String[] wordListThree = {"Ino", "Hinata", "Temari"};
 
+		int listOneLength = wordListOne.length;
+		int listTwoLength = wordListTwo.length;
+		int listThreeLength = wordListThree.length;
 
+		java.util.Random randomGenerator = new java.util.Random();
+		int ListOneRandomWord = randomGenerator.nextInt(listOneLength);
+		int ListTwoRandomWord = randomGenerator.nextInt(listTwoLength);
+		int ListThreeRandomWord = randomGenerator.nextInt(listThreeLength);
+
+		String finalPhrase = (wordListOne[ListOneRandomWord] + ", " + wordListTwo[ListTwoRandomWord] + ", and " + wordListThree[ListThreeRandomWord] + ".");
+
+		System.out.println("In order to find the one piece Luffy needs " + finalPhrase);
 	}
 }
