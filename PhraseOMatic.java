38738e94e67fe508e2d1f2aa5f5dc9fd2aa71356
Adam <olmpy@hotmail.com>
2023-12-26 17:45:59 -0700

    edit PhraseOMatic.java

---
+++ PhraseOMatic.java
@@ -0,0 +1,15 @@
+/* 
+	Author Adam. 
+	Date: Dec_26_2023
+	Title: Phrase-O-Matic
+	About: An app that makes 3 lists of of words and then randomly picks up one word from each of the three lists, then print out the results to the console. 
+*/
+public class PhraseOMatic {
+	Public static void main(String []args) {
+		String[] wordListOne = {"Fido", "Zues", "Jesus"};
+		String[] wordListTwo = {"Saski", "Naruto", "Gara"};
+		String[] wordListThree = {"Ino", "Hinata", "Temari"};
+
+
+	}
+}
